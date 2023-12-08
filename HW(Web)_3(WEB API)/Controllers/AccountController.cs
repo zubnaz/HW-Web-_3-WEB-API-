904b99017e5a62474cb456b2623bd3a9b0833709
zubnaz <nzubar4@gmail.com>
2023-12-08 20:43:55 +0200

    TEst

--- HW(Web)_3(WEB API)/Controllers/AccountController.cs
+++ HW(Web)_3(WEB API)/Controllers/AccountController.cs
@@ -64,16 +64,16 @@ namespace HW_Web__3_WEB_API_.Controllers
 
             return Ok(JsonSerializer.Serialize(await iAS.IsAdmin()));
         }
-        [HttpPut("Buy")]
-        /*public async Task<IActionResult> Buy(int id)
-        {
-            iAS.Buy(id);
-            return Ok();
-        }
-        [HttpGet("user-autos")]
-        public async Task<IActionResult> GetUsersAuto()
-        {
-            return Ok(JsonSerializer.Serialize(await iAS.GetUsersAuto()));
-        }*/
+        /* [HttpPut("Buy")]
+         public async Task<IActionResult> Buy(int id)
+         {
+             iAS.Buy(id);
+             return Ok();
+         }
+         [HttpGet("user-autos")]
+         public async Task<IActionResult> GetUsersAuto()
+         {
+             return Ok(JsonSerializer.Serialize(await iAS.GetUsersAuto()));
+         }*/
     }
 }
